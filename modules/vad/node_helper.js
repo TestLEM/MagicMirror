c266c7fffe481c8bfd757b3088d9dfcdffe0879c
danhee <danhee@wisefour.com>
2018-02-21 13:56:00 +0900

    Changed variable, function name and to use pause instead of stop

--- modules/vad/node_helper.js
+++ modules/vad/node_helper.js
@@ -5,7 +5,7 @@
  * MIT Licensed.
  */
 
-const NodeHelper = require("node_helper");
+const NodeHelper = require('node_helper');
 const mic = require('mic');
 const fs = require('fs');
 
@@ -19,17 +19,17 @@ module.exports = NodeHelper.create({
 	 * 사용하려면 16000 / 채널 1 / 16 bit의 파일로 저장해야한다. 따라서 현재 맥에서는 올바른 음성
 	 * 인식 결과를 얻을 수 없다. 나중에 아래 라이브러리를 사용하여 맥에서도 사용 가능하도록 해야 한다.
 	 * https://www.npmjs.com/package/pcm-convert
-	 * TODO(danhee): recording, vocieStarted의 변수 사용 시 동시 접근 문제를 해결해야 함.
+	 * TODO(danhee): listening, recording의 변수 사용 시 동시 접근 문제를 해결해야 함.
 	 */
 	micInstance: mic({
 		rate: '16000',
 		bitwidth: 16,
 		channels: '1',
-		debug: true,
+		debug: false,
 		exitOnSilence: 6
 	}),
+	listening: false,
 	recording: false,
-	voiceStarted: false,
 	micInputStream: null,
 	outputFileStream: null,
 
@@ -38,12 +38,10 @@ module.exports = NodeHelper.create({
 	},
 
 	socketNotificationReceived: function(notification, payload) {
-		console.log('socketNotificationReceived: ' + notification);
 		switch(notification) {
 			case 'CONNECTED':
 				console.log('vad module connected');
 				this.initMic();
-				this.startRecord();
 				return;
 			default:
 				return;
@@ -53,54 +51,82 @@ module.exports = NodeHelper.create({
 	initMic: function() {
 		const self = this;
 		this.micInputStream = this.micInstance.getAudioStream();
-		this.outputFileStream = fs.WriteStream('output.raw');
 
 		this.micInputStream.on('data', function(data) {
-			console.log("Recieved Input Stream: " + data.length);
-			if (self.isHearingVoice(data) && self.recording) {
-				console.log("onVoiceStart");
-				self.voiceStarted = true;
+			console.log('Recieved Input Stream: ' + data.length);
+			if (self.isHearingVoice(data) && self.listening && !self.recording) {
+				console.log('Recording started');
+				self.recording = true;
+				self.outputFileStream = fs.WriteStream('output.raw');
 				self.micInputStream.pipe(self.outputFileStream);
 			}
 		});
 
 		this.micInputStream.on('silence', function() {
-			console.log("Got SIGNAL silence");
-			self.sendData();
+			if (self.recording) {
+				console.log('Recording stopped');
+				self.recording = false;
+				self.pauseListening();
+				while (true) {
+					if (!self.listening) {
+						self.sendData();
+						break;
+					}
+				}
+				self.resumeListening();
+			}
 		});
 
 		this.micInputStream.on('error', function(err) {
-			cosole.log("Error in Input Stream: " + err);
+			cosole.log('Error in Input Stream: ' + err);
 		});
+
+		this.micInputStream.on('startComplete', function() {
+			console.log('Listeneing started');
+			self.listening = true;
+		});
+
+		this.micInputStream.on('resumeComplete', function() {
+			console.log('Listeneing resume');
+			self.listening = true;
+		});
+
+		this.micInputStream.on('pauseComplete', function() {
+			console.log('Listeneing puase');
+			self.listening = false;
+		});
+
+		this.micInputStream.on('stopComplete', function() {
+			console.log('Listeneing stopped');
+			self.listening = false;
+		});
+	
+		this.micInstance.start();
 	},
 
 	sendData: function() {
-		if (this.voiceStarted) {
-			this.voiceStarted = false;
-			this.stopRecord();
-			console.log("Stop and Send Data and start Recoding");
-			this.sendSocketNotification('GET_SPEECH', {
-				filename: 'output.raw',
-				encoding: 'LINEAR16',
-				sampleRateHertz: '16000'
-			});
-			this.startRecord();
+		console.log('Send speech file');
+		this.sendSocketNotification('GET_SPEECH', {
+			filename: 'output.raw',
+			encoding: 'LINEAR16',
+			sampleRateHertz: '16000'
+		});
+	},
+
+	resumeListening: function() {
+		if (!this.listening) {
+			console.log('Request resume listeneing');
+			this.micInstance.resume();
 		}
 	},
 
-	startRecord: function() {
-		const self = this;
-		if (!self.recording) {
-			self.recording = true;
-			self.micInstance.start();
-		}
-	},
-
-	stopRecord: function() {
-		const self = this;
-		if (self.recording) {
-			self.recording = false;
-			self.micInstance.stop();
+	pauseListening: function() {
+		if (this.listening) {
+			console.log('Request pause listeneing');
+			this.micInstance.pause();
+			this.micInputStream.unpipe(this.outputFileStream);
+			this.outputFileStream.close();
+			this.outputFileStream = null;
 		}
 	},
 
@@ -111,6 +137,7 @@ module.exports = NodeHelper.create({
 			}
 			sample = (data[i+1] << 8 | data[i] & 0xff);
 			if (sample > this.AMPLITUDE_THRESHOLD) {
+				console.log('sampe:' + sample);
 				return true;
 			}
 		}
